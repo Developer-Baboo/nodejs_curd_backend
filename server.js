214e4d7ee9c09a1334f547870235743736e428bd
Developer Baboo <babooheerani@gmail.com>
2023-11-28 16:19:54 +0500

    Made Api

--- server.js
+++ server.js
@@ -5,6 +5,21 @@ import cors from 'cors';
 const app = express();
 app.use(cors());
 
-app.listen(8010, () => {
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'crud',
+})
+
+app.get('/', (req, res) => {
+    const sql = "SELECT * FROM student";
+    db.query(sql, (err, result) => {
+        if (err) return res.json({ Message: "Error inside server" });
+        return res.json(result);
+    })
+})
+
+app.listen(8081, () => {
     console.log('listening on http://localhost:8010');
 });
