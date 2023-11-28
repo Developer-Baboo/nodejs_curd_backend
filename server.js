ece968270bc9ef6a80c76b227d47b138ed4f5ae9
Developer Baboo <babooheerani@gmail.com>
2023-11-28 17:23:02 +0500

    added created functionality

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 const db = mysql.createConnection({
     host: 'localhost',
@@ -16,6 +17,19 @@ app.get('/', (req, res) => {
     const sql = "SELECT * FROM student";
     db.query(sql, (err, result) => {
         if (err) return res.json({ Message: "Error inside server" });
+        return res.json(result);
+    })
+})
+
+//create functionality
+app.get('/student', (req, res) => {
+    const sql = "INSERT INTO student (`NAME`, `EMAIL`) VALUES(?)";
+    const values = [
+        req.body.name,
+        req.body.email,
+]
+    db.query(sql, [values], (err, result) => {
+        if (err) return res.json(err);
         return res.json(result);
     })
 })
